7c89b9d83529a282f4643903c527ceb671e031ff
clock <1084991652clock@gmail.com>
2016-02-04 10:39:00 +0800

    完成Translucent System Bar特性的代码编写

--- src/main/java/com/clock/systemui/ui/base/BaseActivity.java
+++ src/main/java/com/clock/systemui/ui/base/BaseActivity.java
@@ -2,6 +2,7 @@ package com.clock.systemui.ui.base;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Window;
 
 /**
  * Created by Clock on 2016/2/3.
@@ -11,5 +12,6 @@ public class BaseActivity extends AppCompatActivity{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
     }
 }
