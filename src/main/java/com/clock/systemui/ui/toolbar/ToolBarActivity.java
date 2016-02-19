3e82c8117730a10b3ebd27e8937fab854b5091f2
clock <1084991652clock@gmail.com>
2016-02-19 11:28:14 +0800

    完成Toolbar的最佳实践代码

--- src/main/java/com/clock/systemui/ui/toolbar/ToolBarActivity.java
+++ src/main/java/com/clock/systemui/ui/toolbar/ToolBarActivity.java
@@ -22,13 +22,13 @@ public class ToolBarActivity extends BaseActivity {
         toolbar.setNavigationIcon(R.mipmap.ic_drawer_home);//第一个图标
         toolbar.setLogo(R.mipmap.ic_launcher);//第二个图标
 
-        //toolbar.setTitle("Title");//设置主标题
+        toolbar.setTitle("Title");//设置主标题
         toolbar.setTitleTextColor(getResources().getColor(android.R.color.white));//设置主标题颜色
         toolbar.setTitleTextAppearance(this, R.style.Theme_ToolBar_Base_Title);//修改主标题的外观，包括文字颜色，文字大小等
 
-        //toolbar.setSubtitle("Subtitle");//设置子标题
+        toolbar.setSubtitle("Subtitle");//设置子标题
         toolbar.setSubtitleTextColor(getResources().getColor(android.R.color.darker_gray));//设置子标题颜色
-        toolbar.setSubtitleTextAppearance(this , R.style.Theme_ToolBar_Base_Subtitle);//设置子标题的外观，包括文字颜色，文字大小等
+        toolbar.setSubtitleTextAppearance(this, R.style.Theme_ToolBar_Base_Subtitle);//设置子标题的外观，包括文字颜色，文字大小等
 
     }
 
